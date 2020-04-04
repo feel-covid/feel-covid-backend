67a5ed9f402c9e43e6922b29e2c4dae29137ae2c
Sason Braha <sasonbraha@gmail.com>
2020-04-04 23:57:16 +0300

    Added cors middleware, fixed validation error in statsController.ts

--- lib/controllers/statsController.ts
+++ lib/controllers/statsController.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
 import statsService from '../services/statsService';
 import { createStatPayloadValidator } from '../validators/createStatPayloadValidator';
+import { StatusCodeEnum } from '../@types/enums';
 
 /**
  * @method POST
  */
 const handleAddStats = async (req: Request, res: Response) => {
 	try {
-		await createStatPayloadValidator.validateAsync(req.body);
+		try {
+			await createStatPayloadValidator.validateAsync(req.query);
+		} catch (ex) {
+			return res.status(StatusCodeEnum.BAD_REQUEST).send(ex.message);
+		}
 
 		await statsService.addStats(req.body);
 
--- lib/middlewares/index.ts
+++ lib/middlewares/index.ts
@@ -2,11 +2,13 @@ import { Application } from 'express';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import { validateApiKey } from './validateApiKey';
 
 export const setupMiddlewares = (app: Application) => {
-	app.use(bodyParser.json());
 	app.use(helmet());
+	app.use(cors());
+	app.use(bodyParser.json());
 	app.use(compression());
 	app.post('*', validateApiKey);
 };
